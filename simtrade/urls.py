2bb4c0327a5c7b9d5692662d001b81ef49c414b0
driftboy <251296652@qq.com>
2026-05-22 16:25:47 +0800

    feat: add document list page with routing

--- simtrade/urls.py
+++ simtrade/urls.py
@@ -3,6 +3,22 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 from django.shortcuts import render
+from django.http import HttpResponse
+from apps.documents.models import Document, DocumentTemplate
+
+# 单证管理临时视图
+def document_create(request):
+    """单证创建页面 - 临时实现"""
+    templates = DocumentTemplate.objects.filter(is_active=True)
+    return render(request, 'documents/create.html', {'templates': templates})
+
+def document_preview(request, id):
+    """单证预览页面 - 临时实现"""
+    try:
+        document = Document.objects.get(pk=id)
+    except Document.DoesNotExist:
+        return HttpResponse('单证不存在', status=404)
+    return render(request, 'documents/preview.html', {'document': document})
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -22,4 +38,7 @@ urlpatterns += [
     path('market/', lambda r: render(r, 'products/market.html'), name='market'),
     path('transactions/', lambda r: render(r, 'transactions/list.html'), name='transaction-list'),
     path('transactions/<int:id>/', lambda r, id: render(r, 'transactions/detail.html'), name='transaction-detail'),
+    path('documents/', lambda r: render(r, 'documents/list.html'), name='document-list'),
+    path('documents/create/', document_create, name='document-create'),
+    path('documents/<int:id>/preview/', document_preview, name='document-preview'),
 ]
